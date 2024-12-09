9d2b3e2471717f76e147ba2c0b970111ef7471bd
mreed6 <msreed1811@gmail.com>
2024-12-08 21:36:54 -0500

    Push Changes 32

--- forms/contact.php
+++ forms/contact.php
@@ -2,7 +2,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'reed@sowitsolutions.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = "../assets/vendor/php-email-form/php-email-form.php" )) {
     echo 'File found';
     include( $php_email_form );
   } else {
